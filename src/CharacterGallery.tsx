8a15959d3acfb0910fbd353eb418bfa134a8df5c
chris_yooo <hhxtreams@gmail.com>
2022-10-20 15:47:44 +0200

    add search input

--- src/CharacterGallery.tsx
+++ src/CharacterGallery.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react';
+import React, {ChangeEvent, useEffect, useState} from 'react';
 import {nanoid} from "nanoid";
 import styled from "styled-components";
 import CharacterCard from "./CharacterCard";
@@ -14,6 +14,7 @@ type Character = {
 export default function CharacterGallery() {
 
     const [characters, setCharacters] = useState<Character[]>([])
+    const [searchInput, setSearchInput] = useState<string>("")
 
     useEffect(() => {
         axios.get("https://rickandmortyapi.com/api/character")
@@ -22,23 +23,42 @@ export default function CharacterGallery() {
             })
     }, []);
 
+    const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+        // console.log(event.target.value);
+        setSearchInput(event.target.value);
+        // console.log(searchInput);
+    }
 
-    return (
-        <StyledMain>
-            {
-                characters.map(character => {
-                    return <CharacterCard
-                        name={character.name}
-                        img={character.image}
-                        origin={character.origin.name}
-                        key={nanoid()}/>
-                })
-            }
-        </StyledMain>
+    const filterCharacter = characters
+        .filter((character) => character.name.toLowerCase().includes(searchInput.toLowerCase()));
+
+    console.log(filterCharacter);
+
+    return (<>
+            <StyledInput onChange={handleChange}/>
+
+            <StyledMain>
+                {
+                    filterCharacter.map(character => {
+                        return <CharacterCard
+                            name={character.name}
+                            img={character.image}
+                            origin={character.origin.name}
+                            key={nanoid()}/>
+                    })
+                }
+            </StyledMain>
+        </>
     );
 }
 
 const StyledMain = styled.main`
+  display: flex;
+  flex-flow: row wrap;
+  justify-content: center;
+`
+
+const StyledInput = styled.input`
   display: flex;
   flex-flow: row wrap;
   justify-content: center;
